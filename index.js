5384c6a43121c02870c6ded9cfcee75e527fa996
Brandon Parks <brandonparks2020@gmail.com>
2021-08-23 18:59:55 -0500

    final commit

--- index.js
+++ index.js
@@ -1,63 +1,104 @@
+// const submitHandler = (event) => {
+//   event.preventDefault();
+
+//   const form = document.querySelector("#park-form");
+//   const formData = new FormData(form);
+//   // Keep track of if any errors are found
+//   let hasErrors = false;
+
+//   formData.forEach((value, key) => {
+//     let errorId = `#${key}-error`;
+//     if (value.trim() === "") {
+//       document.querySelector(errorId).style.display = "block";
+//       hasErrors = true;
+//     } else {
+//       document.querySelector(errorId).style.display = "none";
+//     }
+//   });
+
+
+//   // if there are no errors
+//   if (!hasErrors) {
+//     //create a new element
+//     const parkSection = document.createElement("section");
+
+//     // add the park class
+//     parkSection.classList.add("park-display");
+
+//     // construct the HTML for this element
+//     const content = `
+//     <h2>${formData.get("name")}</h2>
+//     <div class="location-display">${formData.get("location")}</div>
+//     <div class="description-display">${formData.get("description")}</div>
+//     <button class="rate-button" title="Add to Favourites">&#9734;</button>
+//     <div class="stats">
+//       <div class="established-display stat">
+//         <h3>Established</h3>
+//         <div class="value">${formData.get("established")}</div>
+//       </div>
+//       <div class="area-display stat">
+//         <h3>Area</h3>
+//         <div class="value">${formData.get("area")}</div>
+//       </div>
+//       <div class="rating-display stat">
+//         <h3>Rating</h3>
+//         <div class="value">${formData.get("rating")}</div>
+//       </div>
+//     </div>
+//     `;
+
+//     // set the innerHTML
+//     parkSection.innerHTML = content;
+
+//     //append to the main element
+//     document.querySelector("main").appendChild(parkSection);
+//   }
+// };
+
+// refactoed submitHandler since we have direct access to data.js
 const submitHandler = (event) => {
   event.preventDefault();
 
   const form = document.querySelector("#park-form");
   const formData = new FormData(form);
+
   // Keep track of if any errors are found
   let hasErrors = false;
 
-  formData.forEach((value, key) => {
-    let errorId = `#${key}-error`;
-    if (value.trim() === "") {
-      document.querySelector(errorId).style.display = "block";
-      hasErrors = true;
-    } else {
-      document.querySelector(errorId).style.display = "none";
-    }
-  });
+  // Validation code skipped for brevity
+  // ...
 
-  // if there are no errors
+  // If there are no errors
   if (!hasErrors) {
-    //create a new element
-    const parkSection = document.createElement("section");
+    // Create an empty object
+    const park = {
+      name: formData.get("name"),
+      location: formData.get("location"),
+      description: formData.get("description"),
+      established: formData.get("established"),
+      area: formData.get("area"),
+      rating: formData.get("rating"),
+    };
 
-    // add the park class
-    parkSection.classList.add("park-display");
+    parks.push(park);
 
-    // construct the HTML for this element
-    const content = `
-    <h2>${formData.get("name")}</h2>
-    <div class="location-display">${formData.get("location")}</div>
-    <div class="description-display">${formData.get("description")}</div>
-    <button class="rate-button" title="Add to Favourites">&#9734;</button>
-    <div class="stats">
-      <div class="established-display stat">
-        <h3>Established</h3>
-        <div class="value">${formData.get("established")}</div>
-      </div>
-      <div class="area-display stat">
-        <h3>Area</h3>
-        <div class="value">${formData.get("area")}</div>
-      </div>
-      <div class="rating-display stat">
-        <h3>Rating</h3>
-        <div class="value">${formData.get("rating")}</div>
-      </div>
-    </div>
-    `;
-
-    // set the innerHTML
-    parkSection.innerHTML = content;
-
-    //append to the main element
-    document.querySelector("main").appendChild(parkSection);
+    render();
   }
 };
 
 // function to handler favorite button clicks
+// const favoriteButtonClickHandler = (event) => {
+//   const park = event.target.parentNode;
+//   park.style.backgroundColor = "#c8e6c9";
+// };
+// REFACTORED since addEventListener from main() is now attached to main element
+// check if the event.target was a "BUTTON"
+// if "BUTTON" target the parentNode of that "BUTTON" then change the style.backgroundColor
 const favoriteButtonClickHandler = (event) => {
-  const park = event.target.parentNode;
-  park.style.backgroundColor = "#c8e6c9";
+  if (event.target && event.target.nodeName == "BUTTON") {
+    const park = event.target.parentNode;
+    park.style.backgroundColor = "#c8e6c9";
+  }
 };
 
 // function for sorting by name
@@ -72,7 +113,7 @@ const favoriteButtonClickHandler = (event) => {
 //     return 0;
 //   }
 // };
-
+// REFACTORED sortByName
 const sortByName = (parkA, parkB) => {
   const parkAName = parkA.name;
   const parkBName = parkB.name;
@@ -85,16 +126,24 @@ const sortByName = (parkA, parkB) => {
   }
 };
 
-// function for sorting by rating
+// // function for sorting by rating
+// const sortByRating = (parkA, parkB) => {
+//   const parkARating = parseFloat(
+//     parkA.querySelector(".rating-display > .value").innerText
+//   );
+//   const parkBRating = parseFloat(
+//     parkB.querySelector(".rating-display > .value").innerText
+//   );
+//   return parkBRating - parkARating;
+// };
+
 const sortByRating = (parkA, parkB) => {
-  const parkARating = parseFloat(
-    parkA.querySelector(".rating-display > .value").innerText
-  );
-  const parkBRating = parseFloat(
-    parkB.querySelector(".rating-display > .value").innerText
-  );
-  return parkBRating - parkARating;
-};
+    const parkARating = parkA.rating
+    const parkBRating = parkB.rating
+
+    return parkARating - parkBRating
+}
+
 
 // // function for handling the nameSorter click
 // const nameSorterClickHandler = (event) => {
@@ -180,12 +229,19 @@ const main = () => {
   ratingSorter.addEventListener("click", ratingSorterClickHandler);
 
   // select all the buttons for all the parks
-  const allBtns = document.querySelectorAll(".rate-button");
+  // const allBtns = document.querySelectorAll(".rate-button");
 
   // iterate the list of buttons and add an event handler to each
-  allBtns.forEach((btn) => {
-    btn.addEventListener("click", favoriteButtonClickHandler);
-  });
+  // allBtns.forEach((btn) => {
+  //   btn.addEventListener("click", favoriteButtonClickHandler);
+  // });
+  // REFACTORED -> render() will remove button addEventListener
+  // --> so we INSTEAD attatch the addEventListener to main
+  // when the button is clicked so is main so this will not be removed!
+  // Select all the buttons for all the parks
+  const main = document.querySelector("main");
+  // Add event handler to the main
+  main.addEventListener("click", favoriteButtonClickHandler);
 
   // get the form element
   const form = document.querySelector("#park-form");
